d1a9be88a5e1fb883cda75ae62cbc70b6a3b5fa6
Adil Naqvi <adil.naqvi@convertdigital.com.au>
2023-02-15 19:06:30 +0500

    misc: investigating broken ISR

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,27 +5,29 @@ import { Commercetools } from '~/lib/commercetools';
 import { FALLBACK_IMAGE } from '~/utils/constants';
 
 export default async function HomePage() {
-	const { data } = await Commercetools.getProducts({ limit: 100 });
+	// const { data } = await Commercetools.getProducts({ limit: 100 });
 
-	// NOTE: `getBlurDataUrl` won't ever reject so we can safely use `Promise.all`
-	const blurDataUrls = await Promise.all(
-		data.products.map(async product => {
-			const blurDataUrl = await getBlurDataUrl(product.mainImage?.src);
-			return {
-				id: product.id,
-				blurDataUrl,
-			};
-		}),
-	);
+	// // NOTE: `getBlurDataUrl` won't ever reject so we can safely use `Promise.all`
+	// const blurDataUrls = await Promise.all(
+	// 	data.products.map(async product => {
+	// 		const blurDataUrl = await getBlurDataUrl(product.mainImage?.src);
+	// 		return {
+	// 			id: product.id,
+	// 			blurDataUrl,
+	// 		};
+	// 	}),
+	// );
 
-	const products = data.products.map(product => {
-		const blurDataUrl =
-			blurDataUrls.find(url => url.id === product.id)?.blurDataUrl ||
-			FALLBACK_IMAGE;
+	// const products = data.products.map(product => {
+	// 	const blurDataUrl =
+	// 		blurDataUrls.find(url => url.id === product.id)?.blurDataUrl ||
+	// 		FALLBACK_IMAGE;
 
-		// Add the blurDataURL to the normalised product
-		return { ...product, mainImage: { ...product.mainImage, blurDataUrl } };
-	});
+	// 	// Add the blurDataURL to the normalised product
+	// 	return { ...product, mainImage: { ...product.mainImage, blurDataUrl } };
+	// });
+
+	await new Promise(r => setTimeout(r, 3000)); // Simulate an API call
 
 	const now = new Date().toISOString();
 
@@ -34,7 +36,7 @@ export default async function HomePage() {
 			<div className='mb-4'>
 				<PageGenerationTimeBanner generatedAt={now} />
 			</div>
-			<div className='grid grid-flow-row grid-cols-4 gap-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'>
+			{/* <div className='grid grid-flow-row grid-cols-4 gap-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'>
 				{products?.map((product, index) => {
 					return (
 						<ProductCard
@@ -45,25 +47,25 @@ export default async function HomePage() {
 						/>
 					);
 				})}
-			</div>
+			</div> */}
 		</div>
 	);
 }
 
 export const revalidate = 300; // Revalidate every 5 minutes
 
-async function getBlurDataUrl(src: string) {
-	try {
-		const { base64 } = await generateBase64EncodedDataUrl(
-			// The current project has 2 sizes of images: large and small. But only
-			// the large images are used in the product for obvious reasons. Since
-			// we want the data URL to be very light and small, we use the small
-			// version of the image to generate the data URL.
-			src.replace('_large', '_small'),
-		);
-		return base64;
-	} catch (error: unknown) {
-		console.warn('[getBlurDataUrl]:', error);
-		return null;
-	}
-}
+// async function getBlurDataUrl(src: string) {
+// 	try {
+// 		const { base64 } = await generateBase64EncodedDataUrl(
+// 			// The current project has 2 sizes of images: large and small. But only
+// 			// the large images are used in the product for obvious reasons. Since
+// 			// we want the data URL to be very light and small, we use the small
+// 			// version of the image to generate the data URL.
+// 			src.replace('_large', '_small'),
+// 		);
+// 		return base64;
+// 	} catch (error: unknown) {
+// 		console.warn('[getBlurDataUrl]:', error);
+// 		return null;
+// 	}
+// }
