62f1768260de7df67abb999792ce981716e39ee2
Adil Naqvi <adil.naqvi@convertdigital.com.au>
2023-04-01 11:27:54 +0500

    perf: replace outdated @next/font with recommended next/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { ApolloProvider } from '@apollo/client';
-import { Inter } from '@next/font/google';
+import { Inter } from 'next/font/google';
 import { PropsWithChildren, useEffect } from 'react';
 
 import {
