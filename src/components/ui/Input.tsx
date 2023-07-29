27dfc26175e34d57236f3fcd91f3d69c6b82566b
Adil Naqvi <adil.naqvi@convertdigital.com.au>
2023-07-30 03:00:05 +0500

    wip: testing fix for serverless-function-size error on Vercel

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -2,33 +2,51 @@
 
 import React, { useId } from 'react';
 
+import { EyeIcon, EyeOffIcon } from '~/components/icons';
 import { Label } from '~/components/ui';
+import { useToggle } from '~/hooks';
 import { cx } from '~/utils';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+	type?: 'text' | 'password';
 	label?: string;
 	error?: string;
 	disabled?: boolean;
 }
 
 const baseClassNames =
-	'peer w-full py-2 text-base bg-white text-gray-900 border border-gray-400 disabled:border-gray-300 rounded px-3 disabled:cursor-not-allowed disabled:bg-gray-100 font-normal focus-visible:ring-2 ring-offset-2 focus-visible:ring-primary outline-none transition-colors disabled:text-gray-400 placeholder-gray-400';
+	'peer w-full py-2 text-base bg-white text-gray-900 border border-gray-400 disabled:border-gray-300 rounded px-3 disabled:cursor-not-allowed disabled:bg-gray-100 font-normal focus-visible:ring-2 ring-offset-2 focus-visible:ring-primary outline-none disabled:text-gray-400 placeholder-gray-400';
 
 const errorClassNames =
-	'border-error focus-visible:border-error focus-visible:ring-error placeholder-error/40 text-error disabled:border-error disabled:text-error/40';
+	'border-error focus-visible:ring-error placeholder-error/40 text-error disabled:border-error disabled:text-error/40 disabled:border-error/40';
 
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(
 	(props, ref) => {
 		const randomId = useId();
+		const [showPassword, toggleShowPassword] = useToggle(false);
+
 		const {
 			id,
 			label,
 			// Using '' as default value to avoid uncontrolled input warning
-			value = '',
+			defaultValue = '',
 			error = undefined,
+			disabled = false,
+			type: originalType = 'text',
 			className,
 			...rest
 		} = props;
+
+		const currentType =
+			originalType === 'password' && showPassword ? 'text' : originalType;
+
+		const iconClassNames = cx('h-6 w-6 stroke-1', {
+			'stroke-gray-500': !disabled && !error,
+			'stroke-gray-400': disabled && !error,
+			'stroke-error': !disabled && error,
+			'stroke-error/60': disabled && error,
+		});
+
 		return (
 			<div
 				className={cx(
@@ -42,13 +60,35 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
 				>
 					{error ? <p className='text-sm text-error'>{error}</p> : null}
 				</div>
-				<input
-					className={cx(baseClassNames, !!error && errorClassNames)}
-					ref={ref}
-					id={id || randomId}
-					value={value}
-					{...rest}
-				/>
+				<div className='relative w-full'>
+					<input
+						className={cx(baseClassNames, !!error && errorClassNames)}
+						ref={ref}
+						id={id || randomId}
+						defaultValue={defaultValue}
+						disabled={disabled}
+						type={currentType}
+						{...rest}
+					/>
+					{originalType === 'password' ? (
+						<button
+							aria-label='Show password'
+							aria-controls={id || randomId}
+							onClick={toggleShowPassword}
+							className={cx(
+								'absolute right-3 top-[9px] disabled:cursor-not-allowed',
+								!!error && 'focus-visible:outline-error',
+							)}
+							disabled={disabled}
+						>
+							{showPassword ? (
+								<EyeOffIcon className={iconClassNames} />
+							) : (
+								<EyeIcon className={iconClassNames} />
+							)}
+						</button>
+					) : null}
+				</div>
 				{label ? (
 					<Label
 						error={!!error}
